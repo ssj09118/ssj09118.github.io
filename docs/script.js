61353af27074fb279cfd0da2c352ed5fe0f261ab
ssj09118 <ssj09118@gmail.com>
2025-05-18 09:59:40 +0000

    EZ

--- docs/script.js
+++ docs/script.js
@@ -2,13 +2,43 @@ const larrow = document.querySelector('.larrow');
 const rarrow = document.querySelector('.rarrow');
 const carousel = document.querySelector('.carousel');
 
-let carousel_left = 0;
+let carousel_index = 0; // 현재 몇 번째 이미지가 맨 왼쪽에 있는지
+
+function update_carousel() {
+    // 여기에 코드 추가
+    // rarrow.disabled = true; 등 활용
+    carousel.style.marginLeft = carousel_index * -387 + 'px';
+    
+    if (carousel_index === 0) {
+        larrow.disabled = true;
+    } else if (carousel_index === 4) {
+        rarrow.disabled = true;
+    } else { rarrow.disabled = false; larrow.disabled = false; }
+}
 
 rarrow.addEventListener('click', function () {
-    carousel_left -= 387;
-    carousel.style.marginLeft = carousel_left + 'px';
+    carousel_index += 1;
+    update_carousel();
 });
 larrow.addEventListener('click', function () {
-    carousel_left += 387;
-    carousel.style.marginLeft = carousel_left + 'px';
+    carousel_index -= 1;
+    update_carousel();
 });
+
+update_carousel();
+
+
+// rarrow.addEventListener('click', function () {
+//     if (carousel_index >= 4)
+//         return;
+//     carousel_index += 1;
+//     update_carousel();
+// });
+// larrow.addEventListener('click', function () {
+//     if (carousel_index > 0) {
+//         carousel_index -= 1;
+//         update_carousel();
+//     }
+// });
+
+
